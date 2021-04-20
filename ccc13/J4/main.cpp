dd001c933e74515513c356dffbeac01ed2fa1cb5
GreyAerin <bramogus@gmail.com>
2021-04-20 17:03:15 -0400

    Solved CCC '13 J4, Edited file names.

--- ccc13/J4/main.cpp
+++ ccc13/J4/main.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long
+const ll MAXN = 2e6+5;
 
 using namespace std;
 
-int main()
-{
-    int t, c, ct;
-    int *cTime = NULL;
-    scanf("%d %d", &t, &c);
-    cTime = new int[c];
-    for (int i = 0; i < t; i++)
-    {
-        scanf("%d", &t);
-        cTime[i] = t;
+int main(){
+  cin.sync_with_stdio(0);
+  cin.tie(NULL);
+  int n, r, c, s =0;
+  cin >> n >> r;
+  vector<int> t;
+  for(int i = 0; i < r; i++){
+    cin >> c;
+    t.push_back(c);
+  }
+  sort(t.begin(), t.end());
+  for(int i = 1; i <= t.size() ; i++ ){
+    s += t[i-1];
+    if(s > n){
+      cout << i-1 << endl;
+      return 0;
     }
+  }
+  cout << t.size();
+
+  return 0;
 }
